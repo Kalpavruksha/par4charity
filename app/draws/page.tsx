f8ef64d96dc0e29df0b501e8e9fa32ce0bc89517
kalpavruksha G <154514800+Kalpavruksha@users.noreply.github.com>
2026-04-28 00:31:10 +0530

    Update page.tsx

--- app/draws/page.tsx
+++ app/draws/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import Link from 'next/link'
-
+ 
 export default function DrawsPublicPage() {
     const now = new Date()
     const nextDraw = new Date(now.getFullYear(), now.getMonth() + 1, 0)
